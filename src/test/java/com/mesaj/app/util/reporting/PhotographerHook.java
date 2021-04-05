83a525ecd0662f78857b11723c275f517bad702d
Carlos Ríos Ríos <crioss83@gmail.com>
2021-04-04 19:07:25 -0500

    Se amplia la funcionalidad de TakeScreeshot para poder configurar en el archivo de propiedades, diferentes condiciones para tomar screen shots, tales como: - En cada paso (everyStep) - Al finalizar el escenario (endOfScenario) - Al fallar un escenario (failingScenario) - No tomar screenshot (none).

--- src/test/java/com/mesaj/app/util/reporting/PhotographerHook.java
+++ src/test/java/com/mesaj/app/util/reporting/PhotographerHook.java
@@ -1,12 +1,14 @@
 package com.mesaj.app.util.reporting;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.UUID;
 
@@ -15,13 +17,31 @@ public class PhotographerHook {
     @Autowired
     private WebDriver webDriver;
 
+    @Value("${take.screenshot.condition:none}")
+    private TakeScreenshotCondition takeScreenshotCondition;
+
     @After
-    public void takeScreenShot(Scenario scenario){
+    public void takeScreenshotAfterScenario(Scenario scenario){
+        if(TakeScreenshotCondition.endOfScenario == takeScreenshotCondition)
+            takeScreenShot(scenario);
+    }
+
+    @AfterStep
+    public void takeScreenshotAfterEveryStep(Scenario scenario){
+        if(TakeScreenshotCondition.everyStep == takeScreenshotCondition)
+            takeScreenShot(scenario);
+    }
+
+    @After
+    public void takeScreenshotAfterFailingScenario(Scenario scenario){
+        if(scenario.isFailed() &&  TakeScreenshotCondition.failingScenario == takeScreenshotCondition)
+            takeScreenShot(scenario);
+    }
+
+    private void takeScreenShot(Scenario scenario){
         try{
-            if (scenario.isFailed()){
-                final byte[] screenshot = ((TakesScreenshot)webDriver).getScreenshotAs(OutputType.BYTES);
-                scenario.embed(screenshot, "image/png", UUID.randomUUID().toString());
-            }
+            final byte[] screenshot = ((TakesScreenshot)webDriver).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png", UUID.randomUUID().toString());
         }
         catch (WebDriverException wde){
             System.err.println("There was an error taking the screenshot: " + wde.getMessage());
